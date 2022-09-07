322765bc665a35671021acf04727b72738e358e2
Colin Dorman <csdorman@gmail.com>
2022-09-06 20:07:48 -0400

    Finished Permissions video

--- examples/permissions/index.js
+++ examples/permissions/index.js
@@ -1,10 +1,17 @@
 const getPermissions = document.getElementById("get-permissions")
 const permissionStatus = document.getElementById("status")
 
-getPermissions.addEventListener("click", async () => {
-	const permissionResults = await navigator.permissions.query({
-		name: "geolocation",
+if ("permissions" in navigator) {
+	getPermissions.disabled = false // remove disabled label from button HTML
+
+	// Add event listener if API is supported
+	getPermissions.addEventListener("click", async () => {
+		const permissionResults = await navigator.permissions.query({
+			name: "clipboard-read",
+		})
+		console.log("Permission results", permissionResults)
+		permissionStatus.innerText = permissionResults.state
 	})
-	console.log("Permission results", permissionResults)
-	permissionStatus.innerText = permissionResults.state
-})
+} else {
+	PermissionStatus.innerText = "Your browser won't work with this. UPGRADE!"
+}
