5530b25a3c11f3e6b1f35825f46fad48e6301881
Colin Dorman <csdorman@gmail.com>
2022-09-06 20:18:16 -0400

    Finished Paste functionality

--- examples/clipboard/index.js
+++ examples/clipboard/index.js
@@ -3,10 +3,20 @@ const copyButton = document.getElementById("copy")
 // Select copy input
 const copyField = document.getElementById("copy-input")
 
-// Event listener for button
+// Event listener for copy button
 copyButton.addEventListener("click", async () => {
 	// Copy functionality
 	let value = copyField.value
 	await navigator.clipboard.writeText(value)
 	alert(`Copied ${value} to the clipboard!`) //alert on copy
+})
+
+const pasteInput = document.getElementById("paste-input")
+const pasteButton = document.getElementById("paste")
+
+pasteButton.addEventListener("click", async () => {
+	if ("readText" in navigator.clipboard) {
+		const value = await navigator.clipboard.readText()
+		pasteInput.value = value
+	}
 })
