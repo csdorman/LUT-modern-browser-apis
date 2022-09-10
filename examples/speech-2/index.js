f0212e4de67b79843eb4c474d1f1c7022a800bd4
Colin Dorman <csdorman@gmail.com>
2022-09-10 09:44:35 -0400

    Pitch change functionality working.

--- examples/speech-2/index.js
+++ examples/speech-2/index.js
@@ -3,12 +3,26 @@ const speakBtn = document.getElementById('speak')
 const stopBtn = document.getElementById('stop')
 const textBox = document.getElementById('speak-text')
 const voiceSelect = document.getElementById('voice')
-const speedRate = document.getElementById('speed')
-const pitchRate = document.getElementById('pitch')
+const speed = document.getElementById('speed')
+let speedRate = speed.value
+const pitch = document.getElementById('pitch')
+let pitchRate = pitch.value
 
 speakBtn.addEventListener('click', () => {
 	console.log("Button clicked:", speakBtn)
 	const utterance = new SpeechSynthesisUtterance(textBox.value)
 	console.log("Speech utterance", utterance)
+	console.log(speed, speedRate, pitch, pitchRate)
+	utterance.speed = speedRate
+	utterance.pitch = pitchRate
 	synth.speak(utterance)
 })
+
+pitch.onchange = function () {
+	pitchRate = pitch.value
+}
+
+speed.onchange = function () {
+	speedRate = speed.value
+}
+
